63c6f2eda1621650241d158549a0074eea1637e1
martigimeno <marti.gimeno@wallbox.com>
2024-09-09 02:10:18 +0200

    new godot vesion with assets

--- html/index.service.worker.js
+++ html/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1725485165|1029878415';
+const CACHE_VERSION = '1725837651|4700750208';
 /** @type {string} */
 const CACHE_PREFIX = 'AKOProto-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
