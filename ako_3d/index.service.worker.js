ca45f6a0d96146c14730ab3a4cdaac5ea2dcbaf3
martigimeno <marti@merceorti.com>
2024-09-09 12:46:22 +0200

    remove logs

--- ako_3d/index.service.worker.js
+++ ako_3d/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1725877243|323869175';
+const CACHE_VERSION = '1725878728|257946404';
 /** @type {string} */
 const CACHE_PREFIX = 'AKOProto-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
