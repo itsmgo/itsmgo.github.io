ea8de698f9f0732b5ffd1d2bbd3d3616aa8fc056
martigimeno <marti@merceorti.com>
2024-09-15 18:48:43 +0200

    ako_v3 js

--- ako_v3/index.service.worker.js
+++ ako_v3/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1726411867|4941728374';
+const CACHE_VERSION = '1726418881|11956185573';
 /** @type {string} */
 const CACHE_PREFIX = 'AKOv3-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
